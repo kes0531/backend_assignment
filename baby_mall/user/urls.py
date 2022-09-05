e4171f7f7779da92b105d271bb87c3fe6f0974d0
Na Changwon <include_stdio.h@kakao.com>
2022-09-05 14:31:22 +0900

    user mixins 적용
    * user 프로필 조회 mixins 적용
    + user 프로필 수정 API 생성

--- baby_mall/user/urls.py
+++ baby_mall/user/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import UserProfileAPI, UserSignupAPI, UserLoginAPI
+from .views import UserProfileAPI, UserSignupAPI, UserLoginAPI, UserProfileEditAPI
 
 urlpatterns = [
     path("user/<str:userid>/", UserProfileAPI.as_view()),
+    path("user/<str:userid>/edit", UserProfileEditAPI.as_view()),
     path("user/signup/", UserSignupAPI.as_view()),
     path("user/login/", UserLoginAPI.as_view()),
 ]
--- baby_mall/user/views.py
+++ baby_mall/user/views.py
@@ -1,28 +1,49 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.authtoken.models import Token
+# from rest_framework.authtoken.models import Token
+from rest_framework import mixins, generics
 
 from .models import Profile
 from .serializers import UserSerializer
 
 from django.contrib.auth import authenticate
 
-class UserProfileAPI(APIView) :
-    def get(self, request, userid):
-        user_data = Profile.objects.filter(userid=userid)
-        serializer = UserSerializer(user_data, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+# mixin 사용 전 코드
+# class UserProfileAPI(APIView) :
+#     def get(self, request, userid):
+#         user_data = Profile.objects.filter(userid=userid)
+#         serializer = UserSerializer(user_data, many=True)
+#         return Response(serializer.data, status=status.HTTP_200_OK)
+
+class UserProfileAPI(mixins.RetrieveModelMixin, generics.GenericAPIView) : 
+    queryset = Profile.objects.all()
+    serializer_class = UserSerializer
+    lookup_field = 'userid'
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs) # profile 정보 가져오기
+
+class UserProfileEditAPI(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, generics.GenericAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = UserSerializer
+    lookup_field = 'userid'
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs) 
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs) # profile 수정하기
+
 
 class UserSignupAPI(APIView) :
     def post(self, request):
         serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
+        if serializer.is_valid(): # 유효한 값이라면
+            serializer.save() # DB 저장
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)  
 
 class UserLoginAPI(APIView) :
-    def post(self, request):
-        serializer = UserSerializer(data=request.data)
+    def get(self, request):
+        return Response()
